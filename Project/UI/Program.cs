4226b3aca61d3f3bad0e7048c3ce97d7d8999679
Mo Elsamahi <Elsamahi95@gmail.com>
2022-04-05 17:52:39 -0400

    spelling mistake fix

--- Project/UI/Program.cs
+++ Project/UI/Program.cs
@@ -6,5 +6,5 @@ string connectionString = File.ReadAllText("./connectionString.txt");
 
 //Dependency Injection, Whatever that is 
 IRepository repo = new DBRepository(connectionString);
-ISLBL b1 = new SLBL(repo);
-new Menu(b1).Start();
+ISLBL bl = new SLBL(repo);
+new Menu(bl).Start();
