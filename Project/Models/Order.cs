8e9b4798e8917d97de916986c971667a672252b1
Mo Elsamahi <Elsamahi95@gmail.com>
2022-04-05 23:47:56 -0400

    updated orders and products to work

--- Project/Models/Order.cs
+++ Project/Models/Order.cs
@@ -1,12 +1,60 @@
+using System.ComponentModel.DataAnnotations;
 using Models;
 
-public class Order
+public class Order : Default
 {
-    public int price {get; set;}
+    private double total = 0.00; 
+    private List<Product> cartItems = new List<Product>(); 
+    private int custID = 0;
+    private int storeID = 0;
 
-    public Cart orderList {get; set;}
+//------------------------------------------------------------------
+
+    public DateTime DateCreated { get; set; }
+
+    public void AddTotal (double addPrice)
+    {
+        total += addPrice;
+    } 
+
+    public double Total()
+    {
+        return total;
+    }
+
+    public int CustID {
+        get => custID; 
+        set
+        {
+            if(value <= 0)
+                throw new ValidationException("Something went wrong with custID");
+            
+            custID = value;
+        }
+    }
+
+    public int StoreID {
+        get => storeID; 
+        set
+        {
+            if(value <= 0)
+                throw new ValidationException("Something went wrong with storeID");
+            
+            storeID = value;
+        }
+    }
 
     
 
+    public void AddCartItems (Product adding)
+    {
+        cartItems.Add(adding);
+        AddTotal(adding.Price);
+    }
 
+    public List<Product> CartItem ()
+    {
+        return cartItems;
+    }
+    
 }
--- Project/Models/Product.cs
+++ Project/Models/Product.cs
@@ -28,9 +28,4 @@ public class Product : Default
             price = value;
         }
     }
-
-    public override string ToString()
-    {
-        return $"[{Id}]: {itemName} ";
-    }
 }
